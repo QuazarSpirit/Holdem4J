c5e017a2c2292365b34d5502648a70ae07116bd7
Adrien <contact+github@jeydolen.com>
2025-09-12 16:41:28 +0200

    Try to implement WS connection

--- src/main/java/org/quazarspirit/holdem4j/RoomLogic/Lobby/LobbyServer.java
+++ src/main/java/org/quazarspirit/holdem4j/RoomLogic/Lobby/LobbyServer.java
@@ -47,6 +47,30 @@ public class LobbyServer implements ILobby {
         app.post("/join_game", ctx -> {
             joinGameFromRequest(ctx);
         });
+
+        // Not working TODO: Fix
+        // app.ws("/ws/join_game/{player_uuid}/{game_uuid}", ws -> {
+        // // ws.onConnect(ctx -> {
+        // // System.out.println("Essai de connection");
+        // // String playerUuid = (String) ctx.queryParam("player_uuid");
+        // // IPlayer player = fetchPlayer(UUID.fromString(playerUuid));
+
+        // // String gameUuid = (String) ctx.queryParam("game_uuid");
+        // // Game game = _games.get(UUID.fromString(gameUuid));
+        // // joinGame(player, game);
+        // // });
+
+        // ws.onMessage(ctx -> {
+        // });
+
+        // ws.onClose(ctx -> {
+        // // Disconnect from table
+        // });
+
+        // ws.onError(ctx -> {
+        // System.out.println("Erreur WS : " + ctx.error());
+        // });
+        // });
     }
 
     /**
@@ -95,6 +119,7 @@ public class LobbyServer implements ILobby {
 
             ctx.result("Joined game : " + game);
             joinGame(player, game);
+            // TODO: Make player join through websocket or smth
         } catch (ClassCastException e) {
             ctx.status(500).json(Map.of("error", "Can't join games with specified params"));
         }
